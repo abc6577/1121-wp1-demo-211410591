77e4f3b30b5f2d9cc40e249713935eb83622d272
abc6577 <terrye1992@gmail.com>
2023-09-28 04:20:30 +0800

    W03-P3: Display all menu from <Menu_xx />

--- w03_91_menu/src/pages/App2.js
+++ w03_91_menu/src/pages/App2.js
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import menu_data from "./data";
-import Menu_91 from "./components/Menu_91";
 
 const App = () => {
   const [menu, setMenu] = useState(menu_data);
-  //console.log("menu", menu);
+  console.log("menu", menu);
   return (
     <section className="menu">
       <div className="title">
@@ -32,7 +31,23 @@ const App = () => {
           shakes
         </button>
       </div>
-      <Menu_91 menu={menu} />
+      <div className="section-center">
+        {menu.map((item) => {
+          const { id, title, price, img, desc } = item;
+          return (
+            <article key={id} className="menu-item">
+              <img src={img} alt={title} className="photo" />
+              <div className="item-info">
+                <header>
+                  <h4>{title}</h4>
+                  <h4 className="price">{price}</h4>
+                </header>
+                <p className="item-text">{desc}</p>
+              </div>
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 };
